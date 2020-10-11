dd231d5b96fe75b23a17187982c5ecb60112d804
baopeizhou <baopeizhou@126.com>
2020-10-11 21:14:18 +0800

    dsf

---
+++ src/main/java/org/bob/learn/config/ServerConfig.java
@@ -0,0 +1,13 @@
+package org.bob.learn.config;
+
+import org.springframework.boot.web.embedded.undertow.UndertowServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ServerConfig implements WebServerFactoryCustomizer<UndertowServletWebServerFactory> {
+
+    @Override
+    public void customize(UndertowServletWebServerFactory factory) {
+    }
+}
